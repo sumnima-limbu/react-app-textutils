73e4e3eeb3033c34179cb229129f7f3ce6dfcd3a
sumnima-limbu <sumnimaklimbu@gmail.com>
2023-03-14 15:42:24 +0545

    add extra button functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function TextForm(props) {
   const handleUpClick = () => {
@@ -13,12 +13,33 @@ function TextForm(props) {
     setText(newText);
   };
 
+  const handleCopyClick = () => {
+    console.log("clipboard is clicked");
+    alert("Text is copied to Clipboard.");
+    navigator.clipboard.writeText(text);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  const handleDowClick = () => {
+    console.log("download is clicked");
+    alert("Text is downloaded.");
+    fileDownload(text, download.txt);
+  };
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
   const handleOnChange = (event) => {
     // console.log("On change");
     setText(event.target.value);
   };
 
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
+  const textAreaRef = useRef(null);
   //   text = "New text"; // Wrong way to change the state
   //   setText("new Text"); // Correct way to change the state
   return (
@@ -40,9 +61,21 @@ function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleLowClick}>
           Convert to Lowercase
         </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyClick}>
+          Copy to Clipboard
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleDowClick}>
+          Download
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear Text
+        </button>
       </div>
       <div className="container my-3">
-        <h1>Your text summary</h1>
+        <h2>Your text summary</h2>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
